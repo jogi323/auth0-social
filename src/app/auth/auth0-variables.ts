3a86914f67fe6ab292472affb8a86b7af2cb8489
MrF <fayazbasha974@gmail.com>
2017-06-22 00:46:13 +0530

    first commit

---
+++ src/app/auth/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'YoVnzRgS66epw1GjTpz4ug6F15e127C1',
+  domain: 'fgtfgttf.auth0.com',
+  callbackURL: 'http://localhost:4200/callback'
+};
